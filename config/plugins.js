186c416ca040a8e6b1eec9b741eec404fedf779e
AbdulSamman <sammanab@outlook.de>
2024-06-02 00:47:20 +0200

    images added

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+    // ...add it from cloudinary name, key , secret
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
